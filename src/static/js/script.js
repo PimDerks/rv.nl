11231068a31275fbc7e251b5e34711f627d1f5ba
Pim Derks <info@pimderks.com>
2016-05-04 20:34:17 +0200

    Fixed header effect demo

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,9 +1,24 @@
 (function(){
 
+    var getOffset = function (element) {
+            var pos = {x:0,y:0};
+            if (element.offsetParent) {
+                do {
+                    pos.x += element.offsetLeft;
+                    pos.y += element.offsetTop;
+                    // jshint -W084
+                    console.log(element);
+                } while (element = element.offsetParent);
+            }
+            return pos;
+        };
+
     // window height
     var wHeight = window.innerHeight,
         header = document.querySelector('.post-header'),
         headerInner = document.querySelector('.post-header__inner'),
+        topOfNav = getOffset(document.querySelector('.page-nav ul')).y,
+        topOfHeader = getOffset(document.querySelector('.wrapper--pull-top')).y - topOfNav,
         timer;
 
     // scroll
@@ -13,25 +28,34 @@
             clearTimeout(timer);
         }
 
-        timer = setTimeout(onScroll, 200);
+        timer = setTimeout(onScroll, 10);
 
     });
 
     var onScroll = function(){
         var scroll = document.body.scrollTop,
+            topOfNav = getOffset(document.querySelector('.page-nav ul')).y,
             perc = Math.min(scroll/(wHeight *.5) * 100, 100);
+
 
         // headerInner.style.opacity = 1 - perc/100;
 
         // var percDoc = scroll/document.body.offsetHeight * 100;
         // document.body.style.backgroundPosition = '0 ' + percDoc + '%';
 
+
+        var fixed = true;
+
         var nav = document.querySelector('.page-nav');
-        if(scroll > (wHeight / 4)){
-            nav.classList.add('page-nav--fixed');
-        } else {
-            nav.classList.remove('page-nav--fixed');
-        }
+
+            if (scroll > topOfHeader && fixed) {
+                nav.classList.remove('page-nav--fixed');
+                fixed = false;
+            } else {
+                nav.classList.add('page-nav--fixed');
+                fixed = true;
+            }
+
     };
 
     // random header
