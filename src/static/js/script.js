57e1ebd7d3121320fbbd1bdb937306f38c37ca6c
Pim Derks <info@pimderks.com>
2016-05-21 00:20:57 +0200

    Cleaning up

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -14,40 +14,48 @@
         };
 
     // window height
-    var header = document.querySelector('.post-header'),
+    /* var header = document.querySelector('.post-header'),
+        content = document.querySelector('.wrapper--pull-top'),
         wHeight = window.innerHeight,
-        nav = document.querySelector('.page-nav'),
-        topOfNav = getOffset(document.querySelector('.page-nav ul')).y,
-        topOfHeader = getOffset(document.querySelector('.wrapper--pull-top')).y - topOfNav,
-        timer;
+        nav = document.querySelector('.page-nav'); */
+
+    // measure height of header and offset content
+    /* var heroHeight = header.offsetHeight;
+        content.style.marginTop = (heroHeight - 50) + 'px';
+
+    var topOfNav = getOffset(document.querySelector('.page-nav ul')).y,
+        topOfHeader = getOffset(content).y - topOfNav,
+        timer; */
 
     // scroll
     window.addEventListener('scroll', function(){
 
-        if(timer){
-            clearTimeout(timer);
-        }
+        // if(timer){
+            // clearTimeout(timer);
+        // }
 
-        timer = setTimeout(onScroll, 15);
+        // timer = setTimeout(onScroll, 15);
 
     });
 
     var fixed = true;
     var onScroll = function(){
-        var scroll = document.body.scrollTop,
-            perc = Math.min(scroll/(wHeight *.5) * 100, 100);
+        /* var scroll = document.body.scrollTop,
+            perc = Math.min(scroll/(wHeight *.5) * 100, 100); */
 
         // var percDoc = scroll/document.body.offsetHeight * 100;
 
-        var headerInner = header.querySelector('.post-header__inner').style.opacity = 1 - (perc/100);
+        /* var headerInner = header.querySelector('.post-header__inner').style.opacity = 1 - (perc/100);
         var shouldBeFixed = scroll >= topOfHeader;
             if (shouldBeFixed && fixed) {
                 nav.classList.remove('page-nav--fixed');
+                nav.style.top = heroHeight - 50 + 'px';
                 fixed = false;
             } else if (!shouldBeFixed && !fixed){
                 nav.classList.add('page-nav--fixed');
+                nav.style.top = '';
                 fixed = true;
-            }
+            } */
 
     };
 
