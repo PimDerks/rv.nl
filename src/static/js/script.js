2358729ae719f9070ec89a84c8899de509e0f4a7
Pim Derks <info@pimderks.com>
2016-07-16 13:47:54 +0200

    Events/release etc redesigned.

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,8 +1,23 @@
 (function(){
 
+    var getOffset = function (element) {
+        var pos = {x: 0, y: 0};
+        if (element.offsetParent) {
+            do {
+                pos.x += element.offsetLeft;
+                pos.y += element.offsetTop;
+                // jshint -W084
+            } while (element = element.offsetParent);
+        }
+        return pos;
+
+    };
+
     var hero = document.querySelector('.o-hero'),
         heroInner = document.querySelector('.o-hero__inner'),
         heroHeight = hero.offsetHeight,
+        nav = document.querySelector('.page-nav'),
+        navOffset = getOffset(nav),
         timer;
 
     // scroll
@@ -12,14 +27,23 @@
             clearTimeout(timer);
         }
 
-        timer = setTimeout(onScroll, 25);
+        timer = setTimeout(onScroll, 10);
 
     });
+
+    var update = function(scrollPos, headerPos){
+            var method = scrollPos > headerPos ? 'add' : 'remove';
+            nav.classList[method]('page-nav--fixed');
+    };
 
     var onScroll = function(){
         var scroll = document.body.scrollTop,
             perc = scroll/(heroHeight/100);
 
+        // toggle menu
+        // update(scroll, navOffset.y);
+
+        // animate background in header
         if(perc > 0 && perc <= 100) {
             hero.style.backgroundPosition = '100% ' + perc/2 + '%';
         } else {
@@ -36,6 +60,17 @@
     };
 
 }());
+
+var initMap = function(){
+    var el = document.getElementById('map'),
+        map;
+    if(el){
+        map = new google.maps.Map(el, {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+}
 
 
 
